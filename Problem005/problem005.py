0b71e251a114d2342f10c9c75c5a170d736ad0af
ChristopherSegale <ccsp.segale@gmail.com>
2013-03-04 22:10:50 -0500

    Solving problem 5

--- Problem005/problem005.py
+++ Problem005/problem005.py
@@ -3,29 +3,25 @@
 # Date: 3/3/2013
 
 def is_divisible_by(n, divisor):
-    return not n % divisor
-
-def is_inner_loop_running(smallest_multiple, divisor):
-    if not is_divisible_by(smallest_multiple, divisor):
-	return False
-    else:
-        return True
+    return n % divisor == 0
 
 def smallest_multiple(low, high):
     smallest_multiple = 1
     is_loop = True
+    divisor = low
 
     while is_loop:
-        for x in range(low, high + 1):
-            is_loop_running = is_inner_loop_running(smallest_multiple, x)
-            if not is_loop_running:
-                smallest_multiple+= 1
-                x = low
-            elif is_loop_running and x == high:
+        while divisor < high + 1:
+            if not is_divisible_by(smallest_multiple, divisor):
+                divisor = low
+		smallest_multiple += 1
+            elif is_divisible_by(smallest_multiple, divisor):
                 is_loop = False
-            
+            divisor += 1
+
     return smallest_multiple
 
+# Gives solution, but takes really long to compute it
 def main():
     print smallest_multiple(1, 20)
 
